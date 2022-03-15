d54ac06c64a4c7ed03bb56a9f080330d89379672
BubbleTg <tg_z@qq.com>
2022-03-15 09:56:58 +0800

    feat: save

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+let count: number = 100
+console.log('🚀 ~ file: main.ts ~ line 5 ~ count', count)
+
+count = 222221
+console.log('🚀 ~ file: main.ts ~ line 8 ~ count', count)
+
 createApp(App).mount('#app')
