23acbe74f51119711cd79177ba118e4ce0045d8d
BubbleTg <tg_z@qq.com>
2022-03-15 10:02:44 +0800

    ci: test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin({})]
+  plugins: [vue(), eslintPlugin({
+    cache: false
+  })]
 })
